7905a9a509044dc07d4ee81b548f1ff951ff8a99
chinmay6aug <159870262+chinmay6aug@users.noreply.github.com>
2024-05-11 00:15:44 +0530

    Create guessgame.cpp
    task 1 complete

---
+++ guessgame.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int number, guess, tries = 0;
+	
+	int ch;
+	
+	cout << "Guess My Number Game\n\n";
+	cout<<"Select difficulty level : 1.Easy   2.Intermediate  3.Extreme ";
+	cout<<endl;
+	cout<<"Enter your choice ( 1 or 2 or 3 ) :";
+	cin>>ch;
+	if(ch==1){
+		cout<<"----------------------Easy Mode----------------------"<<endl;
+	srand(time(0)); 
+	number = rand() % 10 + 1; 
+	do
+	{
+		cout << "Guess a number between 1 and 10 : ";
+		cin >> guess;
+		tries++;
+
+		if (guess > number)
+			cout << "Too high!\n\n";
+		else if (guess < number)
+			cout << "Too low!\n\n";
+		else
+			cout << "\nCorrect! You guessed it in " << tries << " guesses!\n";
+	} while (guess != number);
+	}
+	if(ch==2){
+		cout<<"------------------Intermediate Mode------------------"<<endl;
+		srand(time(0)); //seed random number generator
+	number = rand() % 100 + 1; // random number between 1 and 100
+		do
+	{
+		cout << "Guess a number between 1 and 100 : ";
+		cin >> guess;
+		tries++;
+
+		if (guess > number)
+			cout << "Too high!\n\n";
+		else if (guess < number)
+			cout << "Too low!\n\n";
+		else
+			cout << "\nCorrect! You guessed it in" << tries << " guesses!\n";
+	} while (guess != number);
+	}
+	if(ch==3)
+	{
+		cout<<"---------------------Extreme Mode--------------------"<<endl;
+		srand(time(0)); 
+	number = rand() % 1000 + 1; 
+		do
+	{
+		cout << "Guess a number between 1 and 1000 : ";
+		cin >> guess;
+		tries++;
+
+		if (guess > number)
+			cout << "Too high!\n\n";
+		else if (guess < number)
+			cout << "Too low!\n\n";
+		else
+			cout << "\nCorrect! You guessed it in " << tries << " guesses!\n";
+	} while (guess != number);
+	}
+	if(ch<=0 || ch>=4)
+	{
+		cout<<"Invalid choice : restart from beginning"<<endl;
+	}
+
+	return 0;
+}
